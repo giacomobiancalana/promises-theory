96033bb0f30decffb55383fe5a774b4ec076b233
Giacomo Biancalana <biancalanagiacomo20@gmail.com>
2025-04-24 00:03:01 +0200

    Batman

---
+++ src/index.ts
@@ -0,0 +1,53 @@
+type TFile = { nome: string, size: number, };
+const files = getFiles();
+let totalSize = 0;
+
+// (async () => {
+// 	await Promise.all(
+// 		files.map(async (file) => {
+// 			// totalSize += await getSize(file);
+// 			const fileSize = await getSize(file);
+// 			totalSize += fileSize;
+// 		})
+// 	);
+// 	console.log(totalSize);
+// })();
+
+async function gogo() {
+  await Promise.all(
+    files.map(async (file) => {
+      // totalSize += await getSize(file);
+      const fileSize = await getSize(file);
+      totalSize += fileSize;
+    })
+  );
+  console.log(totalSize);
+};
+
+gogo();
+
+
+function getFiles(): TFile[] {
+	return [
+		{
+			nome: 'pippo',
+			size: 4.
+		},
+		{
+			nome: 'pippo',
+			size: 4.
+		},
+		{
+			nome: 'pippo',
+			size: 4.
+		},
+		{
+			nome: 'pippo',
+			size: 4.
+		},
+	];
+};
+
+function getSize(file: TFile): number {
+	return file.size;
+};
